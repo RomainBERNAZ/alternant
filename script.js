57c1e8077c754014aeb1e29f4a0cbdbd03cbaa46
Romain BERNAZ <bernazromain@gmail.com>
2020-08-18 12:10:45 +0200

    premier commit

---
+++ script.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+  // array with texts to type in typewriter
+  var dataText = ["L'ALTERNANT."];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+      document.getElementById("titre_principal").innerHTML = text.substring(0, i + 1) + '<span class="align-middle" aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback)
+      }, 120);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == 'undefined') {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 20000000);
+    }
+    // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+      typeWriter(dataText[i], 0, function () {
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+
+
+
+
+// Fonction qui affiche le compteur
+
+var countDownDate = new Date("Sep 30, 2020 23:59:59").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("secondes").innerHTML = seconds;
+
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("compteur").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+// Affichage des textes avec le scroll 
+
+window.addEventListener('scroll', () => {
+
+  const scrolled = window.scrollY;
+  console/console.log(scrolled);
+  const text = document.getElementById("texte");
+
+
+})
